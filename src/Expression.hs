cc8f26fee90e74ec7ead641949199bb194f40fcc
Quytelda Kahja <quytelda@tamalin.org>
2017-05-20 13:40:04 -0500

    Implement result type.

--- src/Expression.hs
+++ src/Expression.hs
@@ -11,16 +11,27 @@ data Expression = Literal Integer
                 | Application Function [Expression]
                 deriving(Show, Eq)
 
+data Result = IntVal Integer
+            | DecVal Float
+            | NaN
+            deriving(Eq)
+
+instance Show Result where
+  show (IntVal x) = show x
+  show (DecVal x) = show x
+  show NaN        = "NaN"
+
+evaluate :: Expression -> Result
+evaluate (Literal x)           = IntVal x
+evaluate (Decimal x)           = DecVal x
+evaluate (Symbol s)            = NaN
+evaluate Infinity              = NaN
+evaluate (Operation opStr x y) = undefined
+evaluate (Application fn args) = undefined
+
 reduce :: Expression -> Expression
 reduce expr@(Literal x)      = expr
 reduce expr@(Decimal x)      = expr
 reduce expr@(Symbol x)       = expr
 reduce Infinity              = Infinity
 reduce (Application fn args) = undefined
-
-evaluate :: Num a => Expression -> Maybe a
-evaluate (Literal x)           = Just x
-evaluate (Decimal x)           = Just x
-evaluate (Symbol s)            = Nothing
-evaluate Infinity              = Nothing
-evaluate (Application fn args) = undefined
